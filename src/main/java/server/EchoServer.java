b8af2b3e97e5fea085b176f439535a3298841ee9
Yuyi <yuyi365@gmail.com>
2022-05-10 15:36:26 -0400

    refactored code into methods within the EchoServer class

--- src/main/java/server/EchoServer.java
+++ src/main/java/server/EchoServer.java
@@ -5,36 +5,50 @@ import java.io.*;
 
 
 public class EchoServer {
+
+    public static ServerSocket serverSocket = null;
+    public static Socket clientSocket = null;
     public static void start() throws IOException {
-        ServerSocket serverSocket = null;
+
+        openConnection();
+        connectClientSocket();
+
+        PrintWriter serverOutput = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ioStream(serverOutput, clientInput);
+
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public static void openConnection() throws IOException {
 
         try {
             serverSocket = new ServerSocket(1234);
+            System.out.println("[+] Listening for connection on port number 1234...");
         } catch(IOException ie){
             System.out.println(("[-] Cannot listen on port number 1234"));
             System.exit(1);
         }
+    }
 
-        Socket clientSocket = null;
-        System.out.println("[+] Listening for connection...");
-
+    public static void connectClientSocket() throws IOException {
         try {
             clientSocket = serverSocket.accept();
+            System.out.println("[+] Connection is successful");
         } catch(IOException ie) {
             System.out.println("[-] Accept failed");
             System.exit(1);
         }
+    }
 
-        System.out.println("[+] Connection is successful");
+    public static void ioStream(PrintWriter serverOutput, BufferedReader clientInput) throws IOException {
         System.out.println("[+] Listening for an input");
-
-        PrintWriter serverOutput = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         String clientInputLine;
 
         while((clientInputLine = clientInput.readLine()) != null) {
-            System.out.println("Server: " + clientInputLine);
+            System.out.println("Server will echo this back to the client: " + clientInputLine);
             serverOutput.println(clientInputLine);
 
             if (clientInputLine.equals(("bye"))) {
@@ -44,8 +58,6 @@ public class EchoServer {
 
         serverOutput.close();
         clientInput.close();
-        clientSocket.close();
-        serverSocket.close();
+    }
+    }
 
-        }
-}
--- src/test/java/server/EchoServerTest.java
+++ src/test/java/server/EchoServerTest.java
@@ -3,25 +3,17 @@ package server;
 import java.io.*;
 import java.net.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class EchoServerTest {
 
-//    @Test
-//    void start() {
-//    }
-
     @Test
-//    public void testServerConnection() throws IOException {
-//        ServerSocket mockServerSocket = mock(ServerSocket.class);
-//        Socket mockClientSocket = mock(Socket.class);
-//
-//        try{
-//            when(mockServerSocket.accept()).thenReturn(mockClientSocket);
-//        } catch(IOException e){
-//            System.out.println(("[-] Cannot listen on port number 1234"));
-//        }
-//
-//        mockServerSocket.close();
-//    }
+    public void testEchoServerExists() throws IOException {
+        EchoServer.start();
+        Mockito.verify(EchoServer.openConnection(), Mockito.times(1));
+    }
+}
