e5dd878d3d02a73ce91f7cbd31aad4b318d72cf5
Yuyi <yuyi365@gmail.com>
2022-05-11 12:03:20 -0400

    initial commit

--- src/test/java/server/EchoServerTest.java
+++ src/test/java/server/EchoServerTest.java
@@ -9,17 +9,18 @@ import org.mockito.Mockito;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-//class EchoServerTest {
-//
-//    @Test
-//    public void testEchoServerExists() throws IOException {
-//        EchoServer echoServer = mock(EchoServer.class);
-////        Mockito.verify(EchoServer.openConnection(), Mockito.times(1));
-//        assertNotNull(echoServer.openConnection());
-//    }
-//
-//    @Test public void openConnectionHasMessage() {
-//        EchoServer mockEchoServer = mock(EchoServer.class);
-//        assertNotNull("[+] Listening for connection on port 1234", mockEchoServer.openConnection());
-//    }
-//}
+class EchoServerTest {
+
+    @Test
+    public void testEchoServerExists() throws IOException {
+
+        EchoServer echoServer = mock(EchoServer.class);
+//        Mockito.verify(EchoServer.openConnection(), Mockito.times(1));
+        assertNotNull(echoServer.openConnection());
+    }
+
+    @Test public void openConnectionHasMessage() {
+        EchoServer mockEchoServer = mock(EchoServer.class);
+        assertNotNull("[+] Listening for connection on port 1234", mockEchoServer.openConnection());
+    }
+}
