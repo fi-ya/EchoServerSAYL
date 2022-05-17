dd1f4c7549f8a6dd1dcab4e568cd0c47a2e78fe3
fi-ya <safiaali@hotmail.com>
2022-05-17 13:30:48 +0100

    wipl: test multiple thread client connections

--- src/test/java/echoserver/server/EchoServerTest.java
+++ src/test/java/echoserver/server/EchoServerTest.java
@@ -2,12 +2,14 @@ package echoserver.server;
 
 import echoserver.iostream.IOSocketHandler;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.stubbing.answers.ReturnsElementsOf;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -71,5 +73,34 @@ class EchoServerTest {
         verify(mockServerOutput, times(1)).close();
         verify(mockClientInput, times(1)).close();
         verify(mockClientSocket, times(1)).close();
+    }
+
+    @Test
+    public void testMultipleClientsAbleToConnectToServer() throws IOException {
+        StdOutServerLogger serverLogger = new StdOutServerLogger();
+        var echoServer = new EchoServer(serverLogger);
+        ServerSocket mockServerSocket = mock(ServerSocket.class);
+        Socket mockClientSocketOne = mock(Socket.class);
+        Socket mockClientSocketTwo = mock(Socket.class);
+        when(mockServerSocket.accept()).thenReturn(mockClientSocketOne);
+        when(mockServerSocket.accept()).thenReturn(mockClientSocketTwo);
+
+        echoServer.connectClientSocket(mockServerSocket, serverLogger);
+        BufferedReader mockClientInputOne = mock(BufferedReader.class);
+        PrintWriter mockServerOutputOne = mock(PrintWriter.class);
+        when(mockClientInputOne.readLine()).thenAnswer(new ReturnsElementsOf(List.of("hello", "world! ", "bye")));
+        ;
+        IOSocketHandler.clientInputOutputLoop(mockClientInputOne, mockServerOutputOne,mockClientSocketOne);
+
+        echoServer.connectClientSocket(mockServerSocket, serverLogger);
+        BufferedReader mockClientInputTwo = mock(BufferedReader.class);
+        PrintWriter mockServerOutputTwo = mock(PrintWriter.class);
+        when(mockClientInputTwo.readLine()).thenAnswer(new ReturnsElementsOf(List.of("marco", "polo! ", "bye")));
+
+        IOSocketHandler.clientInputOutputLoop(mockClientInputTwo, mockServerOutputTwo,mockClientSocketTwo);
+
+        verify(mockClientInputOne, times(1)).connectClientSocket(mockServerSocket, serverLogger);
+
+
     }
 }
